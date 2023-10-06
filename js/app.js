c370ce4b8055640a88b6444de00ccff2287b9553
Marcus Silva <devsilvamarcus@gmail.com>
2023-10-06 16:41:43 -0300

    Add função para esconder e mostrar do desktop no  Menu Dropdown no JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// função para ativar estilo no cabeçalho ao rolar o mouse
+// Função para ativar estilo no cabeçalho ao rolar o mouse (Scroll)
 function scrollHeader() {
   const nav = document.getElementById("header");
 
@@ -10,3 +10,49 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+// Função para mostrar Menu Dropdown ao clicar na seta
+const dropdownMenu1 = document.getElementById("dropdown-item-1");
+const dropdownMenu2 = document.getElementById("dropdown-item-2");
+const dropdownMenu3 = document.getElementById("dropdown-item-3");
+
+const menuItem1 = document.getElementById("menu-item-1");
+const menuItem2 = document.getElementById("menu-item-2");
+const menuItem3 = document.getElementById("menu-item-3");
+
+function ShowDropdown1(event) {
+  event.preventDefault();
+  dropdownMenu1.classList.toggle("active-dropdown-menu");
+  dropdownMenu2.classList.remove("active-dropdown-menu");
+  dropdownMenu3.classList.remove("active-dropdown-menu");
+}
+
+function ShowDropdown2(event) {
+  event.preventDefault();
+  dropdownMenu1.classList.remove("active-dropdown-menu");
+  dropdownMenu2.classList.toggle("active-dropdown-menu");
+  dropdownMenu3.classList.remove("active-dropdown-menu");
+}
+
+function ShowDropdown3(event) {
+  event.preventDefault();
+  dropdownMenu1.classList.remove("active-dropdown-menu");
+  dropdownMenu2.classList.remove("active-dropdown-menu");
+  dropdownMenu3.classList.toggle("active-dropdown-menu");
+}
+
+function RemoveDropdown() {
+  dropdownMenu1.classList.remove("active-dropdown-menu");
+  dropdownMenu2.classList.remove("active-dropdown-menu");
+  dropdownMenu3.classList.remove("active-dropdown-menu");
+}
+
+// Chamando as funções (ShowDropdown1, 2 e 3) para esconder o Menu Dropdown ao passar mause fora dele.
+menuItem1.addEventListener("click", ShowDropdown1);
+menuItem2.addEventListener("click", ShowDropdown2);
+menuItem3.addEventListener("click", ShowDropdown3);
+
+// Chamando função (RemoveDropdown) para esconder o Menu Dropdown ao passar mause fora dele.
+dropdownMenu1.addEventListener("mouseleave", RemoveDropdown);
+dropdownMenu2.addEventListener("mouseleave", RemoveDropdown);
+dropdownMenu3.addEventListener("mouseleave", RemoveDropdown);
